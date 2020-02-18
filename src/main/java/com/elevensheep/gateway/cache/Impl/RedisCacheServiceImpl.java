39ad1d59d1578810608252ac149bc21ac3f2c16b
jiafan <527223709@qq.com>
2020-02-18 23:05:42 +0800

    update redis

--- src/main/java/com/elevensheep/gateway/cache/Impl/RedisCacheServiceImpl.java
+++ src/main/java/com/elevensheep/gateway/cache/Impl/RedisCacheServiceImpl.java
@@ -3,10 +3,13 @@ package com.elevensheep.gateway.cache.Impl;
 import com.elevensheep.gateway.cache.RouteCacheService;
 import com.elevensheep.gateway.manage.service.Manage;
 import io.vertx.core.Future;
+import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.redis.client.RedisAPI;
+import io.vertx.redis.client.Response;
 import io.vertx.redis.client.impl.RedisClient;
 import java.util.List;
+import java.util.Map;
 
 
 public class RedisCacheServiceImpl implements RouteCacheService{
@@ -21,4 +24,36 @@ public class RedisCacheServiceImpl implements RouteCacheService{
     public Future<List<Manage>> getRoute(String url){
         return null;
     }
+
+	@Override
+	public Future<Response> getValue(String key) {
+        // TODO Auto-generated method stub
+        Promise<Response> promise = Promise.promise();
+        this.redisApi.get(key, handler->{
+            if(handler.succeeded()){
+                promise.complete(handler.result());
+            }else{
+                promise.fail(handler.cause());
+            }
+        });
+		return promise.future();
+	}
+
+	@Override
+	public Future<Response> update(String key, String value) {
+		// TODO Auto-generated method stub
+        return this.redisApi.setnx(key, value);
+	}
+
+	@Override
+	public Future<Response> getMap(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Future<Response> updateMap(Map<String, String> map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- src/main/java/com/elevensheep/gateway/cache/RouteCacheService.java
+++ src/main/java/com/elevensheep/gateway/cache/RouteCacheService.java
@@ -4,8 +4,10 @@ import com.elevensheep.gateway.cache.Impl.RedisCacheServiceImpl;
 import com.elevensheep.gateway.manage.service.Manage;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.redis.client.Response;
 import io.vertx.redis.client.impl.RedisClient;
 import java.util.List;
+import java.util.Map;
 
 
 
@@ -17,4 +19,12 @@ public interface RouteCacheService {
   }
 
   public Future<List<Manage>> getRoute(String url);
+
+  public Future<Response> getValue(String key);
+
+  public Future<Response> update(String key, String value);
+
+  public Future<Response> getMap(String key);
+
+  public Future<Response> updateMap(Map<String, String> map);
 }
